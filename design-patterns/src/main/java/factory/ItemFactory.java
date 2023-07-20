ad2dceb1e3e78e8eab90ef69e5d0ff2cb0aafa7d
TechsOcean <131878273+TechsOcean@users.noreply.github.com>
2023-07-20 11:30:47 +0530

    Added return statement

--- design-patterns/src/main/java/factory/ItemFactory.java
+++ design-patterns/src/main/java/factory/ItemFactory.java
@@ -5,7 +5,8 @@ public class ItemFactory {
     public Item getItemImpl(String name) {
 
         if (name.equalsIgnoreCase("FoodItem")) {
-
+            return new FastfoodItem();
         }
+        return new FoodItem();
     }
 }
